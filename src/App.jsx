5f12fffc830ef2f58eae3ce9ca18122799f94489
namdinh2008 <derricrane@gmail.com>
2025-01-20 21:29:24 +0700

    exam 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardProduct from './components/CardProduct'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <CardProduct />
+    </>
+  )
+}
+
+export default App
